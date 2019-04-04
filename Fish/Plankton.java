1e0a92c57d0c73459c51d65ab70bf93dbb443ef0
maxwelllu3 <blu@lcs.on.ca>
2019-04-04 14:32:15 -0400

    Completed description for Plankton

--- Fish/Plankton.java
+++ Fish/Plankton.java
@@ -1,7 +1,7 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Plankton here.
+ * This class defines a plankton, plankton lives underwater.
  * 
  * @author (your name) 
  * @version (a version number or a date)
